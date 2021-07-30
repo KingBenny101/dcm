cff099b68ff526e088b3fe2f1b209fd6744d6966
KingBenny101 <benstin2727@gmail.com>
2021-07-30 17:59:01 +0530

    Added the Navigator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
+//Put everything that must happen after loading page here
+
 window.addEventListener("load", function () {
   //close button
   var closeButton = document.getElementsByClassName("close")[0];
-  console.log("yolo");
   closeButton.addEventListener("click", function () {
-    console.log("yolo");
-
     window.close();
   });
+
+  //navigator jquery
+  $(".navigator").hover(
+    function () {
+      $("#popup").css("display", "block");
+    },
+    function () {
+      setTimeout(function () {
+        $("#popup").css("display", "none");
+      }, 5000);
+    }
+  );
+
+  //this is totally not needed
   setTimeout(function () {
     window.scroll(screen.width / 2, screen.height / 2);
   }, 1);
 });
+
+// functions
+function hideCmaps(){
+  $('#cmap1').css("display","none");
+  $('#cmap2').css("display","none");
+
+  $('#cmap3').css("display","none");
+
+  $('#cmap4').css("display","none");
+  $('#cmap5').css("display","none");
+
+}
+
+function goToPage(page){
+  hideCmaps();
+  $('#dcm').css("display","block");
+  if(page == 1){
+  $('#dcm').attr("src","")
+
+  }
+
+}
